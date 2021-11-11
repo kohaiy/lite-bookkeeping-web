3af1fc8acd5238561b666bf2681f96b6c6351e12
jsun969 <i@jsun969.cn>
2021-11-11 23:18:27 +0800

    refactor: optimize function component with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import VersionBadge from './components/version-badge';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to <code>bk.kohai.dev</code>, the site is building.</p>
-        <p>欢迎访问 <code>bk.kohai.dev</code>，网站建设中。</p>
-        <a
-          className="App-link"
-          href="https://github.com/kohaiy/lite-bookkeeping-fe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>
+          Welcome to <code>bk.kohai.dev</code>, the site is building.
+        </p>
+        <p>
+          欢迎访问 <code>bk.kohai.dev</code>，网站建设中。
+        </p>
+        <a className="App-link" href="https://github.com/kohaiy/lite-bookkeeping-fe" target="_blank" rel="noopener noreferrer">
           Visit on GitHub
         </a>
       </header>
-      <VersionBadge/>
+      <VersionBadge />
     </div>
   );
-}
+};
 
 export default App;
