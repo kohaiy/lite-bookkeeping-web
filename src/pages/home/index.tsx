4bb99a65c2f28748c223de2c607811badfbdfb87
kohai <916309979@qq.com>
2022-03-08 23:43:38 +0800

    update: UI

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,14 @@ import { getBills } from '@/apis/modules/bill';
 import { GetBillsResp } from '@/apis/modules/bill/get-bills';
 import { useAuth } from '@/router/AuthProvider';
 import { clearToken } from '@/helpers/storage';
+import { Box, Button, Card, CardActionArea, CardContent, Fab, Typography } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { BillTypeEnum } from '@/enums';
+import { formatMoney } from '@/helpers/data';
+
+const getBillType = (type: number) => {
+    return type === BillTypeEnum.BT_INCOME ? 1 : type === BillTypeEnum.BT_PAY ? -1 : 0;
+};
 
 const Home: React.FC = () => {
     const [bills, setBills] = useState<GetBillsResp[]>([]);
@@ -24,27 +32,42 @@ const Home: React.FC = () => {
         auth.setUser();
     };
     return (
-        <div className="h-full overflow-auto">
-            <h1 className="text-3xl font-bold underline">Home</h1>
-            <Link to="/bill-add" className="fixed bottom-2 right-2 w-12 h-12 flex items-center justify-center rounded-full bg-blue-400">
-                <span className="text-4xl text-white leading-none">+</span>
-            </Link>
-            <button onClick={handleLogout}>退出登录</button>
-            <ul className=" mx-auto max-w-xl px-2 pb-20">
+        <div>
+            <Button onClick={handleLogout}>退出登录</Button>
+            <ul className="px-2 pb-20">
                 {bills.map((bill) => (
-                    <li className="flex mb-2 px-4 py-2 rounded bg-white shadow" key={bill.id}>
-                        <div>
-                            <div className=" text-lg font-bold">{bill.billTagName}</div>
-                            <div className=" text-sm text-gray-600">{bill.remarks}</div>
-                            <div className="mt-2 text-xs text-gray-400">{new Date(bill.actionTime).toLocaleDateString()}</div>
-                        </div>
-                        <div className="flex-1 flex flex-col items-end justify-center">
-                            <div>￥{(bill.amount / 100).toLocaleString('zh-CN')}</div>
-                            <div className="text-xs text-gray-600">{bill.billAccountName}</div>
-                        </div>
-                    </li>
+                    <Card sx={{ maxWidth: 400, mb: 1, mx: 'auto' }} key={bill.id}>
+                        <CardActionArea>
+                            <CardContent>
+                                <Box sx={{ display: 'flex' }}>
+                                    <div>
+                                        <Typography sx={{ fontSize: 16, fontWeight: 'bold' }} color="text.primary" gutterBottom>
+                                            {bill.billTagName}
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                            {bill.remarks}
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 12 }} color="text.disabled">
+                                            {new Date(bill.actionTime).toLocaleDateString()}
+                                        </Typography>
+                                    </div>
+                                    <div className="flex-1 flex flex-col items-end justify-center">
+                                        <Typography sx={{ fontSize: 20 }} color={getBillType(bill.billTypeCode) > 0 ? 'error.main' : 'primary.main'}>
+                                            {['-', '', '+'][getBillType(bill.billTypeCode) + 1]}{formatMoney(bill.amount)}
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                                            {bill.billAccountName}
+                                        </Typography>
+                                    </div>
+                                </Box>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 ))}
             </ul>
+            <Fab color="primary" aria-label="add" component={Link} to="/bill-add" sx={{ position: 'fixed', right: 16, bottom: 16 }}>
+                <Add />
+            </Fab>
         </div>
     );
 };
