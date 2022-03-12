25dcf91122ebf05fdd023c695669f18fab91f502
kohai <916309979@qq.com>
2022-03-13 00:18:35 +0800

    update: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
 import * as path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:9000/',
-        // target: 'https://testing-bk.kohai.dev/api/',
-        rewrite: (p) => p.replace(/^\/api/, ''),
-        changeOrigin: true,
-      },
+export default defineConfig(({ mode }) => ({
+    plugins: [react()],
+    server: {
+        proxy: {
+            '^/api': {
+                target: loadEnv(mode, __dirname).VITE_API_URL,
+                rewrite: (p) => p.replace(/^\/api/, ''),
+                changeOrigin: true,
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, './src'),
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, './src'),
+        },
     },
-  },
-});
+}));
